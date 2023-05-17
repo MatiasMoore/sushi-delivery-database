66e873bc120158a6a9fce1f30ebdb08b4e72ce61
MatiasMoore <nikita.birukov.03@mail.ru>
2023-05-17 20:40:12 +0300

    Generic cruds for simplicity and scalability

--- main.py
+++ main.py
@@ -37,15 +37,152 @@ def crudsDialog(dbConnection: MySQLConnection):
     userChoice = askUser(
         "Выберите таблицу\n"
         "1 - блюда\n"
-        "2 - пользователи\n", 1, 2)
+        "2 - пользователи\n"
+        "3 - заказы\n"
+        "4 - доставщики\n"
+        "5 - сушисты\n", 1, 5)
     if userChoice == -1:
         return -1
     res = int(0)
     if userChoice == 1:
-        res = dishesCRUD(dbConnection)
+        res = genericCRUD(dbConnection, 
+        ["блюда", "новое блюдо", "блюдо"], 
+        "dishes", "id_dish", 
+        [ ["name", ""], ["price", ""] ])
     elif userChoice == 2:
-        res = clientsCRUD(dbConnection)
+        res = genericCRUD(dbConnection, 
+        ["пользователей", "нового пользователя", "пользователя"], 
+        "clients", "id_client", 
+        [ ["name", ""], ["phone_number", ""], ["address", ""]])
+    elif userChoice == 3:
+        res = ordersCRUD(dbConnection)
+    elif userChoice == 4:
+        res = genericCRUD(dbConnection, 
+        ["доставщиков", "нового доставщика", "доставщика"], 
+        "deliverymen", "id_deliveryman", 
+        [ ["name", ""], ["surname", ""], ["salary", ""]])
+    elif userChoice == 5:
+        res = genericCRUD(dbConnection, 
+        ["сушистов", "нового сушиста", "сушиста"], 
+        "chefs", "id_chef", 
+        [ ["name", ""], ["surname", ""], ["salary", ""]])
         
+    return res
+
+def newOrderDialog(dbConnection: MySQLConnection, table, idName, params, existingId=None):
+    for pair in params:
+        pair[1] = input(f"Введите значения для столбца { pair[0] }\n")
+        
+    createdId = existingId
+    if existingId == None:
+        sq.addOne(dbConnection, table, params)
+        createdId = sq.getLastId(dbConnection)
+    else:
+        sq.updateOne(dbConnection, table, idName, existingId, params)
+    
+    addDishes = askUser(
+        "Хотите добавить блюда в заказ?\n"
+        "1 - да\n"
+        "2 - нет\n", 1, 2
+    )
+    if addDishes != 2 and addDishes != -1:
+        sq.getAll(dbConnection, "dishes")
+        sum = int(0)
+        while True:
+            dishToAdd = input(
+                "Введите id блюда для добавления в заказ\n"
+                "enter - завершить добавление блюд\n", 
+            )
+            try:
+                dishToAdd = int(dishToAdd)
+            except:
+                sq.updateOne(dbConnection, table, idName, createdId, [["sum",f"{sum}"]])
+                break
+            getRes = sq.getOne(dbConnection, "dishes", "id_dish", dishToAdd)
+            if getRes == -1:
+                print("Блюда с таким id не существует\n")
+                continue
+            info = sq.getOneInfo(dbConnection, "dishes", "id_dish", dishToAdd)
+            price = int(info[2])
+            sum += price
+            sq.addOne(dbConnection, "orders_to_dishes", [["id_order", f"{ createdId }"], ["id_dish", f"{ dishToAdd }"]])
+    
+    addPromo = askUser(
+        "Хотите добавить промокод в заказ?\n"
+        "1 - да\n"
+        "2 - нет\n", 1, 2
+    )
+    if addPromo != 2 and addPromo != -1:    
+        sq.getAll(dbConnection, "special_offers")
+        while True:
+            promoToAdd = input(
+                "Введите id промокода для добавления в заказ\n"
+            )
+            try:
+                promoToAdd = int(promoToAdd)
+            except:
+                print("Неверное значение, попробуйте снова")
+                continue
+            getRes = sq.getOne(dbConnection, "special_offers", "id_special_offer", promoToAdd)
+            if getRes == -1:
+                print("Промокода с таким id не существует, попробуйте снова")
+                continue
+            sq.updateOne(dbConnection, table, idName, createdId, [["id_special_offer", f"{ promoToAdd }"]])
+            break
+    else:
+        sq.setNullParams(dbConnection, table, idName, createdId, ["id_special_offer"])
+    sq.printAllOrdersWithDishes(dbConnection, createdId)
+    print("Созданная запись")
+
+def ordersCRUD(dbConnection: MySQLConnection):
+    option = askUser(
+        "1 - просмотреть заказы\n"
+        "2 - создать новый заказ\n"
+        "3 - изменить заказ\n"
+        "4 - удалить заказ\n", 1, 4)
+    if option == -1:
+        return -1
+    
+    table = "orders"
+    idName = "id_order"
+    params = [ ["date", ""], ["time", ""], ["address", ""]]
+    
+    res = 0
+    
+    if option == 1:
+        sq.printAllOrdersWithDishes(dbConnection)
+        # sq.getAll(dbConnection, table)
+    elif option == 2:
+        res = newOrderDialog(dbConnection, table, idName, params) 
+    elif option == 3:
+        id = input("Введите id записи для редактирования\n")
+        getRes = sq.getOne(dbConnection, table, idName, id, print=False)
+        if getRes == -1:
+            print("Записи с таким id не существует\n")
+            return -1
+        sq.printAllOrdersWithDishes(dbConnection, id)
+        confirm = askUser(
+            "Эта запись будет изменена\n"
+            "1 - изменить\n"
+            "2 - отмена\n", 1, 2)
+        if confirm == 2 or confirm == -1:
+            return -1
+        sq.deleteOne(dbConnection, "orders_to_dishes", "id_order", id)
+        res = newOrderDialog(dbConnection, table, idName, params, id)
+    elif option == 4:
+        id = input("Введите id записи для удаления\n")
+        getRes = sq.getOne(dbConnection, table, idName, id)
+        if getRes == -1:
+            print("Записи с таким id не существует\n")
+            return -1
+        confirm = askUser(
+            "Эта запись будет удалена\n"
+            "1 - удалить\n"
+            "2 - отмена\n", 1, 2)
+        if confirm == 2 or confirm == -1:
+            return -1
+        sq.deleteOne(dbConnection, table, idName, id)
+    
     return res
 
 def dishesCRUD(dbConnection: MySQLConnection):
@@ -67,6 +204,8 @@ def dishesCRUD(dbConnection: MySQLConnection):
         for pair in params:
             pair[1] = input(f"Введите значения для столбца { pair[0] }\n")
         sq.addOne(dbConnection, table, params)
+        sq.getOne(dbConnection, table, idName, sq.getLastId(dbConnection))
+        print("Созданная запись")
     elif option == 3:
         id = input("Введите id записи для редактирования\n")
         getRes = sq.getOne(dbConnection, table, idName, id)
@@ -82,6 +221,8 @@ def dishesCRUD(dbConnection: MySQLConnection):
         for pair in params:
             pair[1] = input(f"Введите новое значения для столбца { pair[0] }\n")
         sq.updateOne(dbConnection, table, idName, id, params)
+        sq.getOne(dbConnection, table, idName, id)
+        print("Созданная запись")
     elif option == 4:
         id = input("Введите id записи для удаления\n")
         getRes = sq.getOne(dbConnection, table, idName, id)
@@ -117,6 +258,8 @@ def clientsCRUD(dbConnection: MySQLConnection):
         for pair in params:
             pair[1] = input(f"Введите значения для столбца { pair[0] }\n")
         sq.addOne(dbConnection, table, params)
+        sq.getOne(dbConnection, table, idName, sq.getLastId(dbConnection))
+        print("Созданная запись")
     elif option == 3:
         id = input("Введите id записи для редактирования\n")
         getRes = sq.getOne(dbConnection, table, idName, id)
@@ -132,6 +275,8 @@ def clientsCRUD(dbConnection: MySQLConnection):
         for pair in params:
             pair[1] = input(f"Введите новое значения для столбца { pair[0] }\n")
         sq.updateOne(dbConnection, table, idName, id, params)
+        sq.getOne(dbConnection, table, idName, id)
+        print("Созданная запись")
     elif option == 4:
         id = input("Введите id записи для удаления\n")
         getRes = sq.getOne(dbConnection, table, idName, id)
@@ -145,6 +290,57 @@ def clientsCRUD(dbConnection: MySQLConnection):
         if confirm == 2 or confirm == -1:
             return -1
         sq.deleteOne(dbConnection, table, idName, id)
+    
+    return 0
+
+def genericCRUD(dbConnection: MySQLConnection, nameList, table, idName, params):
+    option = askUser(
+        f"1 - просмотреть {nameList[0]}\n"
+        f"2 - создать {nameList[1]}\n"
+        f"3 - изменить {nameList[2]}\n"
+        f"4 - удалить {nameList[2]}\n", 1, 4)
+    if option == -1:
+        return -1
+        
+    if option == 1:
+        sq.getAll(dbConnection, table)
+    elif option == 2:
+        for pair in params:
+            pair[1] = input(f"Введите значения для столбца { pair[0] }\n")
+        sq.addOne(dbConnection, table, params)
+        sq.getOne(dbConnection, table, idName, sq.getLastId(dbConnection))
+        print("Созданная запись")
+    elif option == 3:
+        id = input("Введите id записи для редактирования\n")
+        getRes = sq.getOne(dbConnection, table, idName, id)
+        if getRes == -1:
+            print("Записи с таким id не существует\n")
+            return -1
+        confirm = askUser(
+            "Эта запись будет изменена\n"
+            "1 - изменить\n"
+            "2 - отмена\n", 1, 2)
+        if confirm == 2 or confirm == -1:
+            return -1
+        for pair in params:
+            pair[1] = input(f"Введите новое значения для столбца { pair[0] }\n")
+        sq.updateOne(dbConnection, table, idName, id, params)
+        sq.getOne(dbConnection, table, idName, id)
+        print("Созданная запись")
+    elif option == 4:
+        id = input("Введите id записи для удаления\n")
+        getRes = sq.getOne(dbConnection, table, idName, id)
+        if getRes == -1:
+            print("Записи с таким id не существует\n")
+            return -1
+        confirm = askUser(
+            "Эта запись будет удалена\n"
+            "1 - удалить\n"
+            "2 - отмена\n", 1, 2)
+        if confirm == 2 or confirm == -1:
+            return -1
+        sq.deleteOne(dbConnection, table, idName, id)
+        print("Запись удалена")
     
     return 0
 
--- sushiQueries.py
+++ sushiQueries.py
@@ -16,9 +16,18 @@ def create_connection(host_name, user_name, user_password, db_name):
     return connection
 
 # Печать красивой таблицы в консоль
-def printAsTable(cursor: MySQLCursor):
+def printAsTable(cursor: MySQLCursor, maxWidths=None):
     field_names = [i[0] for i in cursor.description] 
-    print(tabulate(cursor.fetchall(), headers=field_names, tablefmt='psql'))
+    table = cursor.fetchall()
+    for i, row in enumerate(table):
+        newRow = []
+        for elem in row:
+            if elem == None:
+                newRow.append("NULL")
+            else:
+                newRow.append(elem)
+        table[i] = newRow
+    print(tabulate(table, headers=field_names, tablefmt='psql', maxcolwidths=maxWidths))
 
 # Получить заказы за промежуток времени и вывести в консоль
 def getOrdersInTimePeriod(connection: MySQLConnection, startDate: str, endDate: str):
@@ -38,6 +47,29 @@ def getDeliveyManByFullName(connection: MySQLConnection, targetName: str, target
 
     cursor.close()
     
+def printAllOrdersWithDishes(connection: MySQLConnection, oneId=None):
+    cursor = connection.cursor()
+    
+    extra = ""
+    if (oneId != None):
+        extra = f"WHERE orders.id_order = { oneId }"
+    
+    cursor.execute(f"SELECT orders.id_order, orders.date, orders.time, orders.sum, orders.address, special_offers.promocode, GROUP_CONCAT(dishes.name) AS dishes_list " "from orders " 
+    "left join special_offers on special_offers.id_special_offer = orders.id_special_offer "
+    "left join orders_to_dishes on orders_to_dishes.id_order = orders.id_order "
+    f"left join dishes on dishes.id_dish = orders_to_dishes.id_dish {extra} GROUP BY orders.id_order ORDER BY orders.id_order")
+    
+    printAsTable(cursor, [None]*6 + [80])
+
+    cursor.close()
+    
+def getLastId(connection: MySQLConnection):
+    cursor = connection.cursor()
+    cursor.execute("SELECT LAST_INSERT_ID()")
+    id = int(cursor.fetchall()[0][0])
+    cursor.close()
+    return id
+    
 def getAll(connection: MySQLConnection, table: str):
     cursor = connection.cursor()
     cursor.execute(f"SELECT * FROM { table }")
@@ -45,15 +77,26 @@ def getAll(connection: MySQLConnection, table: str):
     printAsTable(cursor)
 
     cursor.close()
+
+def getOneInfo(connection: MySQLConnection, table: str, idName: str, id: int):
+    cursor = connection.cursor()
+    queryStr = f"SELECT * FROM { table } WHERE { table }.{ idName } = { id }"
+    cursor.execute(queryStr)
+    info = cursor.fetchall()[0]
+    cursor.close()
+    return info
     
-def getOne(connection: MySQLConnection, table: str, idName: str, id: int):
+def getOne(connection: MySQLConnection, table: str, idName: str, id: int, print=True):
     res = bool()
     cursor = connection.cursor()
     queryStr = f"SELECT * FROM { table } WHERE { table }.{ idName } = { id }"
     cursor.execute(queryStr)
     if cursor.fetchall().__len__() != 0:
         cursor.execute(queryStr)
-        printAsTable(cursor)
+        if print:
+            printAsTable(cursor)
+        else:
+            cursor.fetchall()
         res = 0
     else:
         res = -1
@@ -67,27 +110,35 @@ def addOne(connection: MySQLConnection, table: str, params):
     valuesStr = ""
     for pair in params:
         columnsStr += f"{ pair[0] }, "
-        valuesStr += f"{ pair[1] }, "
-    print(f"INSERT INTO {table}({ columnsStr[0: -2] }) VALUES ({ valuesStr[0: -2] })")
-    # cursor.execute(f"INSERT INTO {table}({ columnsStr[0: -2] }) VALUES ({ valuesStr[0: -2] })")
-    # connection.commit()
+        valuesStr += f"\"{ pair[1] }\", "
+    cursor.execute(f"INSERT INTO {table}({ columnsStr[0: -2] }) VALUES ({ valuesStr[0: -2] })")
+    connection.commit()
     cursor.close()
 
+def setNullParams(connection: MySQLConnection, table: str, idName: str, id: int, params):
+    cursor = connection.cursor()
+    paramsStr = ""
+    for param in params:
+        paramsStr += f"{ param } = NULL, "
+    
+    queryStr = f"UPDATE { table } SET { paramsStr[0: -2] } WHERE { table }.{ idName } = { id }"
+    cursor.execute(queryStr)
+    connection.commit()
+    cursor.close()
+    
 def updateOne(connection: MySQLConnection, table: str, idName: str, id: int, params):
     cursor = connection.cursor()
     paramsStr = ""
     for pair in params:
-        paramsStr += f"{ pair[0] } = { pair[1] }, "
+        paramsStr += f"{ pair[0] } = \"{ pair[1] }\", "
     
     queryStr = f"UPDATE { table } SET { paramsStr[0: -2] } WHERE { table }.{ idName } = { id }"
-    print(queryStr)
-    # cursor.execute(queryStr)
-    # connection.commit()
+    cursor.execute(queryStr)
+    connection.commit()
     cursor.close()
 
 def deleteOne(connection: MySQLConnection, table: str, idName: str, id: int):
     cursor = connection.cursor()
-    print(f"DELETE FROM { table } WHERE { table }.{ idName } = { id }")
-    # cursor.execute(f"DELETE FROM { table } WHERE { table }.{ idName } = { id }")
-    # connection.commit()
+    cursor.execute(f"DELETE FROM { table } WHERE { table }.{ idName } = { id }")
+    connection.commit()
     cursor.close()  
